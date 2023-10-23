c669607dd7592faa3b4533192aa795f68893d5f8
zakisu250 <zakisu250@gmail.com>
2023-10-23 11:09:42 +0300

    Export employee data in a json format

---
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+Fetching todo lists for an employee from a REST API
+"""
+import requests
+import json
+import sys
+
+
+if __name__ == "__main__":
+    apiURL = "https://jsonplaceholder.typicode.com/users"
+    res = requests.get(apiURL)
+    emps = res.json()
+    empDict = {}
+    for emp in emps:
+        emp_id = emp.get('id')
+        emp_name = emp.get('username')
+        url = 'https://jsonplaceholder.typicode.com/users/{}'.format(emp_id)
+        url = url + '/todos'
+        res = requests.get(url)
+        todos = res.json()
+        empDict[emp_id] = []
+        for todo in todos:
+            empDict[emp_id].append({"task": todo.get('title'),
+                                    "completed": todo.get('completed'),
+                                    "username": emp_name})
+    with open("todo_all_employees.json", 'w') as f:
+        json.dump(empDict, f)
