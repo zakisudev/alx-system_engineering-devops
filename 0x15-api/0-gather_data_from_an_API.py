49f2416c0ed99295cf946414960a81b029fa8153
zakisu250 <zakisu250@gmail.com>
2023-10-23 10:50:03 +0300

    Fetch data from an API using requests for employees

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+Fetching todo lists for an employee from a REST API
+"""
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    empId = sys.argv[1]
+    apiURL = "https://jsonplaceholder.typicode.com/users"
+    mainURL = apiURL + "/" + empId
+
+    res = requests.get(mainURL)
+    empName = res.json().get('name')
+
+    todoURL = mainURL + "/todos"
+    res = requests.get(todoURL)
+    todos = res.json()
+    done = 0
+    done_todos = []
+
+    for todo in todos:
+        if todo.get('completed'):
+            done_todos.append(todo)
+            done += 1
+    print("Employee {} is dome with tasks({}/{}):"
+          .format(empName, done, len(todos)))
+    for todo in done_todos:
+        print("\t {}".format(todo.get("title")))
---
+++ 0x15-api/README.md
@@ -0,0 +1,1 @@
+RESTful API accessing in python
